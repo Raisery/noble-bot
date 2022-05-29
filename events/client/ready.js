b3b9a6543ceac6727afccae2ce7448ebdb564615
Lucas Gérard <lucas1.gerard@gmail.com>
2022-05-29 18:43:00 +0200

    correction de l'importation des commandes

--- events/client/ready.js
+++ events/client/ready.js
@@ -5,8 +5,9 @@ module.exports = {
     once: true,
     async execute(client) {        
         client.user.setActivity('test comme dab', { type: "LISTENING" });
-        const devGuild = await client.guilds.cache.get('971152670209486931');
-        devGuild.commands.set(client.commands.map(cmd => cmd));
+        await client.guilds.cache.forEach(guild => {
+            guild.commands.set(client.commands.map(cmd => cmd));
+        });;
         await client.restoreAnnonce();
         Logger.client(` - Connecté en tant que ${client.user.tag}! avec l'id : ${client.user.id}`);
     }
